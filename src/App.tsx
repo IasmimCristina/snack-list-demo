57a456173c7a869c40d52622103a554d3164cfec
IasmimCristina <iaasmimcristinaa@gmail.com>
2025-01-27 20:13:12 -0300

    feat: Greeting

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import Header from './components/Header';
 import './App.css'
 import { User } from './types/User';
+import Greeting from './components/Greeting';
 
 function App() {
   const [user, setUser] = useState<User>({ name: "", isLoggedIn: false });
 
   return (
-    <div className="App">
+
+    <>
       <Header user={user} setUser={setUser} />
-      {/* O conteúdo do App será exibido aqui, dependendo do estado do login */}
-    </div>
+      <div className="app">
+        <Greeting user={user} />
+        {/* etc */}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { User } from '../types/User';
+import { FaPizzaSlice, FaCookieBite } from 'react-icons/fa'; 
+import './Greeting.css';
+
+type GreetingProps = {
+  user: User;
+};
+
+const Greeting = ({ user }: GreetingProps) => {
+  return (
+    <div className="greeting">
+      <div className="greeting__icon">
+        {user.isLoggedIn ? (
+          <FaPizzaSlice size={50} color="var(--color-accent)" />
+        ) : (
+          <FaCookieBite size={50} color="var(--color-accent)" />
+        )}
+      </div>
+      <div className="greeting__message">
+        {user.isLoggedIn ? (
+          <h1 className="greeting__text">Welcome, {user.name}!</h1>
+        ) : (
+          <h1 className="greeting__text">Welcome to SnackList!</h1>
+        )}
+      </div>
+      <p className="greeting__description">
+        {user.isLoggedIn ? (
+          "Explore your favorite snacks and add them to your lists!"
+        ) : (
+          "Login to start adding your favorite snacks."
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default Greeting;
